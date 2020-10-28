fd9dc0e8d147216c98d43958aedf5597f2e08892
Cory Bass <cory.bass.07@gmail.com>
2020-10-27 22:54:52 -0500

    updated highest_and_lowest.py with other possible solutions for study

--- highest_and_lowest.py
+++ highest_and_lowest.py
@@ -1,8 +1,25 @@
-def high_and_low(numbers):
-    new_numbers = []
-    for n in numbers.split():
-        new_numbers.append(int(n))
-    new_list = [str(max(new_numbers)), str(min(new_numbers))]
-    return ' '.join(new_list)
+# def high_and_low(numbers):
+#     new_numbers = []
+#     for n in numbers.split():
+#         new_numbers.append(int(n))
+#     new_list = [str(max(new_numbers)), str(min(new_numbers))]
+#     return ' '.join(new_list)
+#
+#
+# def high_and_low(numbers):
+#     n = map(int, numbers.split(' '))
+#     return "{} {}".format(max(n), min(n))
+#
+#
+# def high_and_low(numbers):
+#   n = map(int, numbers.split(' '))
+#   return str(max(n)) + ' ' + str(min(n))
+
+
+def high_and_low(numbers): #z.
+    nn = [int(s) for s in numbers.split(" ")]
+    return "%i %i" % (max(nn),min(nn))
+
+
 
 print(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")) # "542 -214"
