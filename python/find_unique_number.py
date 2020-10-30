13f7f0aa84431154f32fd1d2e32f80b0c2273988
Cory Bass <cory.bass.07@gmail.com>
2020-10-29 22:35:48 -0500

    find_unique_number.py complete, submitted, passed

---
+++ python/find_unique_number.py
@@ -0,0 +1,9 @@
+def find_uniq(arr):
+    return sum([n for n in arr if arr.index(n) == len(arr) - 1
+                or n != arr[(arr.index(n) - 1)] and n != arr[(arr.index(n) + 1)]])
+
+
+print(find_uniq([1, 1, 1, 2, 1, 1]))  # 2
+print(find_uniq([0, 0, 0.55, 0, 0]))  # 0.55
+print(find_uniq([3, 10, 3, 3, 3]))  # 10
+print(find_uniq([8, 8, 8, 8, 8, 8, 8, 7]))  # 7
